85b9e264a676c79daa1cb3e1a6bc0b8a624bf920
pyordanovdev <plamenyordanovdev@gmail.com>
2025-02-14 11:14:39 +0200

    feat: added views, components, react browser router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
 import StatsBar from '../StatsBar/StatsBar';
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
 function Header() {
   return (
     <div>
-      header
-      <StatsBar />
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/all-posts/'>All Posts</Link>
+          </li>
+          <li>
+            <Link to='/create-post/'>Create Post</Link>
+          </li>
+          <li>
+            <Link to='/about/'>About</Link>
+          </li>
+          <li>
+            <Link to='/user/'>User Profile</Link>
+          </li>
+        </ul>
+        <Button className='login' onClickHandler={() => {}}>
+          Login
+        </Button>
+
+        <Button className='register' onClickHandler={() => {}}>
+          Register
+        </Button>
+      </nav>
+      <StatsBar totalRegisteredUsers={0} totalPostsCreated={0} />
     </div>
   );
 }
